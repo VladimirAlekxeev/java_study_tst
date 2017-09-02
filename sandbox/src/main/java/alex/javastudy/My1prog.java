e6d7d11ff414371a82eb5c26af3291a937497c24
VladimirAlekxeev <va7tereg0@mail.ru>
2017-09-02 19:17:36 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/alex/javastudy/My1prog.java
+++ sandbox/src/main/java/alex/javastudy/My1prog.java
@@ -2,7 +2,29 @@ package alex.javastudy;
 
 public class My1prog {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        zdorovo("World");
+        zdorovo("user");
+        zdorovo("Vova");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+        double a = 4, b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
     }
+
+    public static void zdorovo(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+    // метод вычисления площади квадрата
+    public static double area( double len) {
+        return len * len;
+    }
+    //метод вычисления площади прямоугольника
+    public static  double area(double a, double b){
+        return a * b;
+    }
+
+
+
 }
 
