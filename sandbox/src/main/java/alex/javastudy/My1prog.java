db36d75340743f34a4441791eb19e82946deb0d8
VladimirAlekxeev <va7tereg0@mail.ru>
2017-09-02 23:03:24 +0300

    конец 1 урока

--- sandbox/src/main/java/alex/javastudy/My1prog.java
+++ sandbox/src/main/java/alex/javastudy/My1prog.java
@@ -1,31 +1,24 @@
 package alex.javastudy;
 
 public class My1prog {
+    public My1prog() {
+    }
+
     public static void main(String[] args) {
         zdorovo("World");
         zdorovo("user");
         zdorovo("Vova");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
 
     public static void zdorovo(String somebody) {
         System.out.println("Hello " + somebody + "!");
     }
-    // метод вычисления площади квадрата
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
-    //метод вычисления площади прямоугольника
-    public static  double area(Rectangle r){
-        return r.a * r.b;
-    }
-
-
 
 }
 
